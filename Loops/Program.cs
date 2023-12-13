ae9a29a4ca8795c93c8546d2634ffca2d9fdc767
Hasnat Ahmed Goheer <ch.hsyahmedgoheer@hotmail.com>
2023-12-13 18:09:32 +0500

    completed Loop practice

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int sum = 0;
+            int count = 0 ;
+            int average ;
+            int num;
+            string input =""; ;
+
+
+            do
+            {
+                Console.Write("Please enter the number u want to add(Make sure its between 0 and 20): ");
+                input = Console.ReadLine();
+                if (int.TryParse(input, out num))
+                {
+                    num = int.Parse(input);
+                    if (num == -1)
+                    {
+                        average = sum / count;
+                        Console.WriteLine(average);
+                        break;
+                    }
+                    if (num > 0 && num < 20)
+                    {
+                        sum += num;
+                        count++;
+                    }
+                }
+            }
+            while (input != "-1");
         }
     }
 }
